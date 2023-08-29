1034ec578fb69036bb3b59b88c2bfbe3519fdbaa
franjavi8776 <franjavi871976@gmail.com>
2023-08-29 15:56:39 -0400

    agregando tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!!</h1>
+      <h1 className="font-bold underline">Hello world!!</h1>
     </div>
   );
 }
