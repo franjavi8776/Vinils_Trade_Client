8c96bccaa91bd73d797da670033147b78095559d
jeremingolla <jeremingolla03@gmail.com>
2023-08-29 18:08:32 -0300

    Probando Cambios en mi rama. JM

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div className="App">
+      <Search/>
       <h1 className="font-bold underline">Hello world!!</h1>
     </div>
   );
