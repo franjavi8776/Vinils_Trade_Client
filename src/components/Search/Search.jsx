ebb86b34aa44ecc2b467c26b89106fc8659a21c6
jeremingolla <jeremingolla03@gmail.com>
2023-08-29 17:26:58 -0300

    Prueba: Probando como quedaria. JM

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,10 @@
+const Search = () =>{
+    return(
+        <div>
+            <input type="text" placeholder="Ingrese el nombre de vinilo"/>
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default Search
