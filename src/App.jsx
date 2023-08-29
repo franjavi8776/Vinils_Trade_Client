502d2829eef57d30b7d6f64c129e3bd4e95ac40a
franjavi8776 <franjavi871976@gmail.com>
2023-08-29 19:56:35 -0400

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="font-bold text-3xl">Hello world!!</h1>
+      <h1 className="font-bold text-3xl text-blue-700">Hello world!!</h1>
     </div>
   );
 }
