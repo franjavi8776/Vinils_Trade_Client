32c9a538259d5ee6f7739ad88314c9c36a1b3412
franjavi8776 <franjavi871976@gmail.com>
2023-08-29 20:14:39 -0400

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import Search from "./components/Search/Search";
 function App() {
   return (
     <div className="App">
-      <h1 className="font-bold text-3xl text-blue-700">Hello world!!</h1>
-
-      <Search />
+      <h1 className="font-bold text-3xl">Hello world!!</h1>
     </div>
   );
 }
