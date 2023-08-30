5bbf7753f3fea55efb00d717724ad4c4f873d852
franjavi8776 <franjavi871976@gmail.com>
2023-08-29 20:01:11 -0400

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div className="App">
       <h1 className="font-bold text-3xl text-blue-700">Hello world!!</h1>
+
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,10 @@
+const Search = () => {
+  return (
+    <div class="flex flex-row-reverse p-8 w-full bg-red-700 dark:bg-slate-800 ">
+      <button>Search</button>
+      <input type="text" class=" mr-4 p-2 font-serif w-96 " placeholder="Ingrese el nombre de vinilo" />
+    </div>
+  );
+};
+
+export default Search;
