4fc701b5a02eb89b534d8f5a6655442922ebba11
MiguelSc66 <miguel.scaccia1@gmail.com>
2023-08-31 20:52:44 -0300

    Modificaciones

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
   const dispatch = useDispatch();
   const vinyls = useSelector((state) => state.allVinyls); //trayendo info.
   const [currentPage, setCurrentPage] = useState(1);
-  // const [selectedDecade, setSelectedDecade] = useState("");
+  const [selectedDecade, setSelectedDecade] = useState("");
   const searchByName = useSelector((state) => state.search);
   const pageSize = 10;
   const totalVinyls =
@@ -59,6 +59,8 @@ const Home = () => {
 
   const handleFilter = (event) => {
     const selectedDecades = event.target.value;
+    setSelectedDecade(selectedDecades); // Actualizando el estado con la década seleccionada
+
     let startYear, endYear;
     if (selectedDecades === "2000") {
       startYear = 2000;
@@ -69,42 +71,14 @@ const Home = () => {
     }
     dispatch(filterVinylsByDecade(startYear, endYear));
   };
+
+
   return (
     <div className="w-[100%] h-[92vh]">
-      <div>
-        <select
-          onChange={handleFilter}
-          className="bg-black text-white p-2 rounded"
-        >
-          <option value="">Selecciona una década</option>
-          <option value="1940">1940s</option>
-          <option value="1950">1950s</option>
-          <option value="1960">1960s</option>
-          <option value="1970">1970s</option>
-          <option value="1980">1980s</option>
-          <option value="1990">1990s</option>
-          <option value="2000">2000s en adelante</option>
-        </select>
-      </div>
-      <select onChange={handleOrderByTitle}>
-        <option value="">Ordenar p/Titulo</option>
-        <option value="A">Ascendente</option>
-        <option value="D">Descendente</option>
-      </select>
       <div>
         {VinylsToRender.length === pageSize && <p>Pag {currentPage}</p>}
       </div>
       <button onClick={handleReset}>Reset</button>
-      <select name="genre" onChange={handleGenre} id="">
-        <option value="" disabled>
-          Generos
-        </option>
-        <option value="Funk / Soul">Funk / Soul</option>
-        <option value="Rock">Rock</option>
-        <option value="Electronic">Electronic</option>
-        <option value="Hip Hop">Hip Hop</option>
-        <option value=""></option>
-      </select>
       <div className="w-[60%] h-[46vh] m-auto ">
         <Swiper
           spaceBetween={30}
@@ -186,7 +160,38 @@ const Home = () => {
         </Swiper>
       </div>
       <div className="w-[100%] h-[56vh] flex flex-row">
-        <div className="w-[20%] bg-gray-100"></div>
+        <div className="w-[20%]">
+          <div className="w-[80%] m-auto flex flex-col gap-5">
+            <select
+              onChange={handleFilter}
+              className="bg-black text-white p-2 rounded"
+            >
+              <option value="">Selecciona una década</option>
+              <option value="1940">1940s</option>
+              <option value="1950">1950s</option>
+              <option value="1960">1960s</option>
+              <option value="1970">1970s</option>
+              <option value="1980">1980s</option>
+              <option value="1990">1990s</option>
+              <option value="2000">2000s en adelante</option>
+            </select>
+            <select onChange={handleOrderByTitle} className="bg-black text-white p-2 rounded">
+              <option value="">Ordenar p/Titulo</option>
+              <option value="A">Ascendente</option>
+              <option value="D">Descendente</option>
+            </select>
+            <select name="genre" onChange={handleGenre} id="" className="bg-black text-white p-2 rounded">
+              <option value="" disabled>
+                Generos
+              </option>
+              <option value="Funk / Soul">Funk / Soul</option>
+              <option value="Rock">Rock</option>
+              <option value="Electronic">Electronic</option>
+              <option value="Hip Hop">Hip Hop</option>
+              <option value=""></option>
+            </select>
+          </div>
+        </div>
         <div className="w-[80%] flex">
           <div>
             {currentPage > 1 && <button onClick={handlePreviousPage}>P</button>}
