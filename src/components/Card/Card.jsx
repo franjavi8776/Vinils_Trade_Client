2f96fdc3e1904344c81d31d18e659160cac4f620
franjavi8776 <franjavi871976@gmail.com>
2023-08-31 00:35:45 -0400

    carrusel

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Card = ({ id, title, year, cover_image }) => {
   return (
-    <div className="w-60 h-60 bg-red-100">
+    <div className="w-60 h-70 ">
       <Link to={`/detail/${id}`}>
-        <h2 className="text-sm">{title}</h2>
-        <img className="w-48 h-48" src={cover_image} alt={title} />
+        <h2 className="text-sm h-10">{title}</h2>
+        <img className="w-48 h-48 m-auto" src={cover_image} alt={title} />
         <h2>{year}</h2>
       </Link>
     </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,15 @@ import { useDispatch, useSelector } from "react-redux";
 import Card from "../Card/Card";
 import { getAllVinyls } from "../../redux/actions";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
 const Home = () => {
   const dispatch = useDispatch();
   const vinyls = useSelector((state) => state.vinyls); //trayendo info.
@@ -43,10 +52,89 @@ const Home = () => {
       <div>
         {VinylsToRender.length === pageSize && <p>Pag {currentPage}</p>}
       </div>
-      <div className="w-[100%] h-[46vh] bg-green-100"></div>
-      <div className="w-[100%] h-[56vh] bg-yellow-100 flex flex-row">
+      <div className="w-[60%] h-[46vh] m-auto ">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img
+              className="w-[100%] h-[40vh]"
+              src="/carrusel.jpg"
+              alt="image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-[100%] h-[40vh] "
+              src="/carrusel.jpg"
+              alt="image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-[100%] h-[40vh]"
+              src="/carrusel.jpg"
+              alt="image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-[100%] h-[40vh]"
+              src="/carrusel.jpg"
+              alt="image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-[100%] h-[40vh] "
+              src="/carrusel.jpg"
+              alt="image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-[100%] h-[40vh]"
+              src="/carrusel.jpg"
+              alt="image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-[100%] h-[40vh] "
+              src="/carrusel.jpg"
+              alt="image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-[100%] h-[40vh] "
+              src="/carrusel.jpg"
+              alt="image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-[100%] h-[40vh] "
+              src="/carrusel.jpg"
+              alt="image"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="w-[100%] h-[56vh] flex flex-row">
         <div className="w-[20%] bg-gray-100"></div>
-        <div className="w-[80%] bg-orange-100 flex">
+        <div className="w-[80%] flex">
           <div>
             {currentPage > 1 && <button onClick={handlePreviousPage}>P</button>}
           </div>
