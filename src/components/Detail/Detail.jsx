439eee1273b4ffe24b60ed5c9e564b9d34d775eb
franjavi8776 <franjavi871976@gmail.com>
2023-09-02 22:51:08 -0400

    corrigiendo json

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,38 +1,70 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getVinylDetail } from "../../redux/actions";
+
+import style from "./Detail.module.css";
 
 const Detail = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const detail = useSelector((state) => state.detail);
 
-  console.log(detail);
-
   useEffect(() => {
     dispatch(getVinylDetail(id));
   }, [dispatch, id]);
 
   return (
-    <div className="container mx-auto p-4">
-      <div className="max-w-lg mx-auto bg-white rounded-lg shadow-lg overflow-hidden">
-        <div className=" bg-black bg-opacity-90 p-4">
-          <p className=" text-sm  text-white">ID: {detail.id}</p>
-        </div>
-        <div className="p-4">
-          <h1 className="text-3xl font-semibold mb-2">{detail?.title}</h1>
-          <p className="text-gray-600">Pais: {detail?.country}</p>
-          <p className="text-gray-600">
-            Año: {detail.year ? detail.year : "No especificado"}
-          </p>
-        </div>
-        <div className="p-4">
-          <img
-            src={detail?.cover_image}
-            alt={detail?.title}
-            className="w-full rounded h-[450px]"
-          />
+    <div className="w-[100%] h-[80vh] flex justify-center items-center">
+      <div className="mr-[-90px] z-0">
+        <img
+          className={`rounded-full w-[400px] h-[400px] mt-[40%] ${style["animate-spin"]}`}
+          src="/disco2.jpg"
+          alt="disco"
+        />
+      </div>
+
+      <div className="z-50">
+        <div className="w-[500px] h-[740px] bg-white rounded-lg shadow-lg overflow-hidden">
+          <div className=" bg-black bg-opacity-90 p-4">
+            <p className=" text-sm  text-white">ID: {detail.id}</p>
+          </div>
+          <div className="pl-4 pr-4">
+            <h1 className="text-3xl font-semibold mb-2">{detail?.title}</h1>
+            <h2 className="text-gray-900">Pais: {detail?.country}</h2>
+            <h2 className="text-gray-900">
+              Año: {detail.year ? detail.year : "No especificado"}
+            </h2>
+            <h2 className="text-gray-900">Pais: {detail.country}</h2>
+            <h2 className="text-gray-900">
+              Genero: {detail.genre ? detail.genre : "No especificado"}
+            </h2>
+          </div>
+          <div>
+            <img
+              src={detail?.cover_image}
+              alt={detail?.title}
+              className="w-full h-[430px] border-4 border-black border-opacity-95"
+            />
+          </div>
+          <div className="w-full flex justify-between p-2 ">
+            <h2 className="text-red-900 font-bold text-lg">
+              Stock: <b className="text-black">{detail.stock}</b>
+            </h2>
+            <h2 className="text-red-900 font-bold text-lg">
+              Precio: <b className="text-black">{detail.cost}$us</b>
+            </h2>
+          </div>
+          <Link to="/">
+            <div className=" bg-black flex justify-center items-center h-10 text-white cursor-pointer">
+              <span className="mr-2 hover:text-red-600 transition-colors">
+                Agregar al carrito
+              </span>
+              <button>
+                <img src="/carrito.png" alt="carrito" className="w-5" />
+              </button>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
