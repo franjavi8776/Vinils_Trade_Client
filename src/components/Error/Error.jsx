426179bd9af1365733d4d42f516f148f38bdf6fa
Alejandra Villarroel <calevigu@hotmail.com>
2023-09-03 19:05:02 -0500

    ruta de error css

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,5 +1,16 @@
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-8 rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold text-center text-red-600 mb-4">
+          RUTA NO ENCONTRADA
+        </h1>
+        <p className="text-gray-700 text-center">
+          Lo sentimos, la página que estás buscando no se encuentra disponible.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
