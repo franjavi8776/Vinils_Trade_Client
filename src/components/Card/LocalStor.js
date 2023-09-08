fe6db0a5e8e501c9ecefe2f71097f25e9a1a66e2
franjavi8776 <franjavi871976@gmail.com>
2023-09-08 11:04:46 -0400

    carrito de compras

--- src/components/Card/LocalStor.js
+++ src/components/Card/LocalStor.js
@@ -1,35 +1,35 @@
 export const useLocalStorage = () => {
-    const cartData = localStorage.getItem("cart");
-    return cartData ? JSON.parse(cartData) : [];
+  const cartData = localStorage.getItem("cart");
+  return cartData ? JSON.parse(cartData) : [];
 };
 
 export const saveCartToLocalStorage = (cart) => {
-    localStorage.setItem("cart", JSON.stringify(cart));
+  localStorage.setItem("cart", JSON.stringify(cart));
 };
 
 export const addToCartInLocalStorage = (vinyl) => {
-    const cart = useLocalStorage();
-    const existingProduct = cart.find((item) => item.id === vinyl.id);
+  const cart = useLocalStorage();
+  const existingProduct = cart.find((item) => item.id === vinyl.id);
 
-    if (existingProduct) {
-        existingProduct.quantity += 1; // Si el producto ya existe, aumenta la cantidad
-    } else {
-        vinyl.quantity = 1; // Si es un nuevo producto, establece la cantidad en 1
-        cart.push(vinyl);
-    }
+  if (existingProduct) {
+    existingProduct.quantity += 1; // Si el producto ya existe, aumenta la cantidad
+  } else {
+    vinyl.quantity = 1; // Si es un nuevo producto, establece la cantidad en 1
+    cart.push(vinyl);
+  }
 
-    saveCartToLocalStorage(cart);
+  saveCartToLocalStorage(cart);
 };
 
 // Eliminar un vinilo del carrito en el Local Storage por su ID
 export const removeFromCartInLocalStorage = (vinylId) => {
-    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-    const updatedCart = cart.filter((item) => item.id !== vinylId);
+  const updatedCart = cart.filter((item) => item.id !== vinylId);
 
-    localStorage.setItem("cart", JSON.stringify(updatedCart));
+  localStorage.setItem("cart", JSON.stringify(updatedCart));
 };
 
 export const clearCartInLocalStorage = () => {
-    localStorage.removeItem("cart");
+  localStorage.removeItem("cart");
 };
