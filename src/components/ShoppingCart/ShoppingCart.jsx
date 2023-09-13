6fe448ba0362ea21614c0ec3339582b8440defc2
MiguelSc66 <miguel.scaccia1@gmail.com>
2023-09-13 04:58:25 -0300

    agregadod e cartel

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,22 +1,22 @@
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   removeFromCart,
   increaseItem,
   decreaseItem,
-  postMP, 
+  postMP,
   succesMP,
   pendingMP,
   failureMP,
   clearCart,
 } from "../../redux/actions";
-import { MdOutlineRemoveShoppingCart } from "react-icons/md"
-
-
+import { MdOutlineRemoveShoppingCart } from "react-icons/md";
 
 const ShoppingCart = () => {
+  const [showConfirmation, setShowConfirmation] = useState(false);
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cartItems);
-  const MP = useSelector((state) => state.dataMP)
+  const MP = useSelector((state) => state.dataMP);
   // const stateMP = useSelector((state) => state.stateMP)
   const handleRemoveFromCart = (vinylId) => {
     dispatch(removeFromCart(vinylId));
@@ -41,13 +41,14 @@ const ShoppingCart = () => {
     0
   );
 
+  const handleShowConfirmation = () => {
+    setShowConfirmation(true);
+  };
+
   // const units = cart.length;
 
   //console.log(datos);
 
-  
-  
-  
   const handleMP = () => {
     if (cart.length > 0) {
       const datos = {
@@ -58,10 +59,14 @@ const ShoppingCart = () => {
       console.log(datos);
       dispatch(postMP(datos));
       if (MP.length > 0) {
+        // Redirigir al usuario a Mercado Pago
         window.location.href = MP;
+
+        // Restablecer el estado de showConfirmation
+        setShowConfirmation(false);
       }
     }
-  }
+  };
 
   // if(succesMP) {
   //   dispatch(clearCart())
@@ -72,8 +77,6 @@ const ShoppingCart = () => {
   // if(failureMP) {
   //   return "fallo"
   // }
-
-
 
   return (
     <div
@@ -89,61 +92,87 @@ const ShoppingCart = () => {
           X
         </button>
         <ul className="w-[700px] h-[630px] overflow-y-auto">
-          {cart.length ? (cart.map((item) => (
-            <div
-              key={item.id}
-              className="flex justify-between items-center p-2 border-b"
-            >
-              <img
-                src={item.cover_image}
-                alt={item.title}
-                className="w-20 h-20"
-              />
-              <div className="flex flex-col flex-grow">
-                <span>{item.title}</span>
-                <span>Stock: {item.stock}</span>
-                <span>Precio total: ${item.price * item.cartQuantity}</span>
+          {cart.length ? (
+            cart.map((item) => (
+              <div
+                key={item.id}
+                className="flex justify-between items-center p-2 border-b"
+              >
+                <img
+                  src={item.cover_image}
+                  alt={item.title}
+                  className="w-20 h-20"
+                />
+                <div className="flex flex-col flex-grow">
+                  <span>{item.title}</span>
+                  <span>Stock: {item.stock}</span>
+                  <span>Precio total: ${item.price * item.cartQuantity}</span>
+                </div>
+                <div className="flex gap-2">
+                  <button
+                    onClick={() => handleDecrease(item)}
+                    className="bg-red-500 text-white px-2 rounded"
+                  >
+                    -
+                  </button>
+                  <span>{item.cartQuantity}</span>
+                  <button
+                    onClick={() => handleIncreaseQty(item)}
+                    className="bg-green-500 text-white px-2 rounded"
+                    disabled={item.stock === 0}
+                  >
+                    +
+                  </button>
+                  <button
+                    onClick={() => handleRemoveFromCart(item.id)}
+                    className="bg-red-500 text-white px-2 rounded"
+                  >
+                    Eliminar
+                  </button>
+                </div>
               </div>
-              <div className="flex gap-2">
-                <button
-                  onClick={() => handleDecrease(item)}
-                  className="bg-red-500 text-white px-2 rounded"
-                >
-                  -
-                </button>
-                <span>{item.cartQuantity}</span>
-                <button
-                  onClick={() => handleIncreaseQty(item)}
-                  className="bg-green-500 text-white px-2 rounded"
-                  disabled={item.stock === 0}
-                >
-                  +
-                </button>
-                <button
-                  onClick={() => handleRemoveFromCart(item.id)}
-                  className="bg-red-500 text-white px-2 rounded"
-                >
-                  Eliminar
-                </button>
-              </div>
+            ))
+          ) : (
+            <div className=" flex flex-col justify-center items-center w-[700px] h-[630px]">
+              <MdOutlineRemoveShoppingCart className="w-[150px] h-[150px] text-red-800" />
+              <div className="font-bold text-xl">Carrito vacio</div>
             </div>
-          ))) : (<div className=" flex flex-col justify-center items-center w-[700px] h-[630px]">
-            <MdOutlineRemoveShoppingCart className="w-[150px] h-[150px] text-red-800"/>
-            <div className="font-bold text-xl">Carrito vacio</div>
-            </div>)
-          }
+          )}
         </ul>
         <div className="p-4 text-right w-full h-[130px] absolute bottom-0 border-[1px] border-black dark:border-white">
           <div>Total: ${totalValue}</div>
           <div className=" flex justify-between">
-            <button onClick={handlerButtom} className="bg-red-800 text-white px-4 py-2 rounded mt-4">
+            <button
+              onClick={handlerButtom}
+              className="bg-red-800 text-white px-4 py-2 rounded mt-4"
+            >
               Seguir Comprando
             </button>
-            <button className="bg-green-500 text-white px-4 py-2 rounded mt-4" onClick={handleMP}>
+            <button
+              className="bg-green-500 text-white px-4 py-2 rounded mt-4"
+              onClick={handleShowConfirmation}
+            >
               Finalizar Compra
             </button>
           </div>
         </div>
+        {showConfirmation && (
+          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 h-[100px] w-[530px] -translate-y-1/2 flex items-center text-black from-red-700 to-red-950 p-4 border border-gray-300 shadow-lg z-50 dark:text-white">
+            <p>¿Estás seguro de que deseas finalizar la compra?</p>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white  font-bold py-2 px-4 rounded mr-2"
+              onClick={handleMP}
+            >
+              Sí
+            </button>
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => setShowConfirmation(false)}
+            >
+              No
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
