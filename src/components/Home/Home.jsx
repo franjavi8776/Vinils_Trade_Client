d4f504a26802e5e907d4b5997edff5cb8e0ef905
franjavi8776 <franjavi871976@gmail.com>
2023-09-17 19:48:22 -0400

    reparando error

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -331,9 +331,7 @@ const Home = () => {
             <div className="w-16 h-16 absolute -top-4 -left-4 text-white rounded-full flex justify-center items-center transform rotate-45">
               ★
             </div>
-            <span className="relative z-10 transform -rotate-45 rotate-0">
-              50% OFF
-            </span>
+            <span className="relative z-10 transform -rotate-45">50% OFF</span>
           </div>
         </div>
 
