a4b8237ea2df20b4130863ebaf1dbdef54492fcb
Alejandra Villarroel <calevigu@hotmail.com>
2023-09-17 18:46:33 -0500

    cambios de color

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -90,7 +90,7 @@ const Dashboard = () => {
   return (
     <div className="w-full h-[100vh] bg-slate-200">
       <div className="w-full h-[5vh] pt-5 pl-5">
-        <div className="w-[150px] h-[50px] clip-path-custom bg-slate-400 flex items-center justify-end">
+        <div className="w-[150px] h-[50px] clip-path-custom bg-slate-900 flex items-center justify-end">
           <Link to="/">
             <h1 className="text-white pr-2">Volver al home</h1>
           </Link>
@@ -98,7 +98,7 @@ const Dashboard = () => {
       </div>
       <div className="w-full h-[40vh] flex justify-center  items-center gap-48 ">
         <Link to="/usarios">
-          <div className="w-[200px] h-[200px] bg-slate-400 shadow-lg shadow-black text-3xl flex flex-col justify-center items-center cursor-pointer active:translate-x-1 active:scale-95 gap-1">
+          <div className="w-[200px] h-[200px] bg-slate-900 shadow-lg shadow-black text-3xl flex flex-col justify-center items-center cursor-pointer active:translate-x-1 active:scale-95 gap-1">
             <h1>USUARIOS</h1>
             <button>
               <BiUserCircle className="w-24 h-24" />
@@ -106,7 +106,7 @@ const Dashboard = () => {
           </div>
         </Link>
         <Link to="/vinylsDash">
-          <div className="w-[200px] h-[200px] bg-slate-400 shadow-lg shadow-black text-3xl flex flex-col justify-center gap-3 items-center cursor-pointer active:translate-x-1 active:scale-95">
+          <div className="w-[200px] h-[200px] bg-slate-900 shadow-lg shadow-black text-3xl flex flex-col justify-center gap-3 items-center cursor-pointer active:translate-x-1 active:scale-95">
             <h1>VINILOS</h1>
             <button>
               <BsDiscFill className="w-20 h-20" />
@@ -115,7 +115,7 @@ const Dashboard = () => {
         </Link>
       </div>
       <div className="w-full h-[50vh] flex justify-center items-center  ">
-        <div className="w-[80%] h-[40vh] m-auto flex justify-around items-center border-4 border-black ">
+        <div className="w-[80%] h-[40vh] m-auto flex justify-around items-center border-4 border-black">
           <div className="w-[230px] h-[300px] ">
             <Card>
               <h1 className="text-white">Géneros de Vinilos</h1>
